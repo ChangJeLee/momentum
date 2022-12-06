180ab68b720d83418aa0d678131237ed713d0215
ChangJeLee <prime12415@gmail.com>
2022-12-06 21:19:37 +0900

    20221206

---
+++ app.js
@@ -0,0 +1,16 @@
+//console.log(545454545); // 콘솔에 전달 및 출력
+/* console.log("'lalalallala'");
+console.log(5 + 2);
+console.log(5 / 2);
+console.log(5 * 2); */
+
+const a = 5;
+const b = 2; 
+const myName ="ddung";
+const veryLongVariableName = 0; //자바스크립트 스타일
+const very_long_var_name=0; //파이썬 스타일
+
+console.log(a + b);
+console.log(a / b);
+console.log(a * b);
+console.log("hello " + myName);
