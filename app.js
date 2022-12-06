13917afa1ffda3b136f436f01e12f6c7754666c6
ChangJeLee <prime12415@gmail.com>
2022-12-06 23:03:47 +0900

    20221206-1

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-//console.log(545454545); // 콘솔에 전달 및 출력
-/* console.log("'lalalallala'");
-console.log(5 + 2);
-console.log(5 / 2);
-console.log(5 * 2); */
+// -------------------------------- Objects --------------------------------------------
+ /*const playerNmae ="cjlee";
+ const playerPoints = "1234";
+ const playerHandsome = "false";
+ const playerFat = "thin";
 
-const a = 5;
-const b = 2; 
-const myName ="ddung";
-const veryLongVariableName = 0; //자바스크립트 스타일
-const very_long_var_name=0; //파이썬 스타일
+player.name
+player.points
+player.handsome*/
+//얘를 Objects로 만들어보자 
 
-console.log(a + b);
-console.log(a / b);
-console.log(a * b);
-console.log("hello " + myName);
+// player[0]  == name
+// player[1]  == points  이렇게 써놔야 알 수 있음 
+
+// const player = ["cjlee", 1212, false, "thin"];
+// 위는 cjlee 1212 false 이런게 어떤 값인지 모름
+
+const player = {
+    name:"cjlee",
+    points:10,
+    hansome:true,
+    fat:true
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+player.fat = false;
+console.log(player); // const를 바꾸는게 아니고 Objects에서의 데이터를 바꾸는 것임 그래서 문제 안생겨 
+
+player.lastName = "shit";
+console.log(player);
+
+player.points= player.points + 15;
+console.log(player.points);
+
+
+// -------------------------------- Objects END --------------------------------------------
