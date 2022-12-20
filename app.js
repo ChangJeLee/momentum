2c15df3ae8f1ae4cc63a6aaf228215bc65cecc98
ChangJeLee <prime12415@gmail.com>
2022-12-20 21:56:16 +0900

    20221220

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-// -------------------------------- Objects --------------------------------------------
- /*const playerNmae ="cjlee";
- const playerPoints = "1234";
- const playerHandsome = "false";
- const playerFat = "thin";
+// ----------------------------------------- Return start ---------------------------------------------------
+const calculator =
+{
+    add: function(a, b)
+    {
+        return a + b;
+    },
+    minus: function(a, b)
+    {
+        return a - b;
+    },
+    poweroff: function(a, b)
+    {
+        return a ** b;
+    },
+    devide: function(a, b)
+    {
+        return a / b;
+    }
+}
 
-player.name
-player.points
-player.handsome*/
-//얘를 Objects로 만들어보자 
-
-// player[0]  == name
-// player[1]  == points  이렇게 써놔야 알 수 있음 
-
-// const player = ["cjlee", 1212, false, "thin"];
-// 위는 cjlee 1212 false 이런게 어떤 값인지 모름
-
-const player = {
-    name:"cjlee",
-    points:10,
-    hansome:true,
-    fat:true
-};
-
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-
-player.fat = false;
-console.log(player); // const를 바꾸는게 아니고 Objects에서의 데이터를 바꾸는 것임 그래서 문제 안생겨 
-
-player.lastName = "shit";
-console.log(player);
-
-player.points= player.points + 15;
-console.log(player.points);
+const plusResult = calculator.add(2, 3); // error
+const minusResult = calculator.minus(2, 3);
+const minusResult2 = calculator.minus(plusResult, 10);
+const powerResult = calculator.poweroff(2, 3);
+const devideResult = calculator.devide(powerResult, plusResult);
 
 
-// -------------------------------- Objects END --------------------------------------------
+console.log(plusResult);
+console.log(minusResult);
+console.log(minusResult2);
+console.log(powerResult);
+console.log(devideResult);
+
+const age = 96;
+function calculateKrAge(ageOfForeigner)
+{
+    return ageOfForeigner + 2; //함수에 console.log를 쓰는 일은 없을꺼임
+}
+
+const krAge = calculateKrAge(age);
+console.log(krAge);
+
+// ----------------------------------------- Return end ---------------------------------------------------
